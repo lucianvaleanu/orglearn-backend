c91bfec5d58e64b07fce500b1a0179b025d2eaa9
lucian <valeanuluciangeorge@gmail.com>
2026-05-12 13:11:00 +0300

    modified cross origin

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ const errorHandler = require('./middleware/errorHandler');
 const app = express();
 
 app.use(helmet());
-app.use(cors({ origin: process.env.ORGLEARN_FRONTEND_URL, credentials: true }));
+app.use(cors({ 
+  origin: [process.env.ORGLEARN_FRONTEND_URL, 'http://localhost:3000'], 
+  credentials: true 
+}));
 app.use(express.json());
 app.use(passport.initialize());
 
